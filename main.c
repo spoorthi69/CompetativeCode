f1aff5ef361a9acf5c62a0d70a163d3cebc0ce98
spoorthi69 <79088643+spoorthi69@users.noreply.github.com>
2021-12-11 19:41:44 +0000

    print the zeros at the end of an given array

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
-  printf("Hello World\n");
+  int i,n,count=0,a[20];
+  printf("Enter the array size\n");
+  scanf("%d",&n);
+  printf("Enter the array elements\n");
+  for(i=0;i<n;i++)
+  scanf("%d",&a[i]);
+  for(i=0;i<n;i++)
+  {
+    if(a[i]!=0)
+    {
+    a[count]=a[i];
+    count++;
+    }
+  }
+  while(count<n)
+  {
+    a[count]=0;
+    count++;
+  
+  }
+/*for(i=0;i<n;i++)
+printf("%d ",a[i]);*/
+for(count=0;count<n;count++)
+printf("%d ",a[count]);
+
   return 0;
 }
